5934c950a1f448985dfe5396986981a760eb8969
Ilan <ilan@gmail.com>
2023-08-18 00:03:59 +0530

    add comment server on

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app=express();
+const port=3000;
+
+let Tasks=[]; 
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+
+app.post("/add",(req,res)=>{
+    Tasks.push(req.body["task"]);
+    res.render("index.ejs",{givenJobs:Tasks});
+})
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+app.listen(port,()=>{
+    console.log("Listening on port 3000... Server On"); //Add comment server on
+})
